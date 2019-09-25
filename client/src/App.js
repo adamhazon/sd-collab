7adc941f905244f4aff96130d1b7893736487f4b
Adam Hazon <adamhazon@adam.localdomain>
2019-09-25 16:07:29 +0200

    Add basic style & menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,13 @@
-import React, { Component } from "react";
-import socketIOClient from "socket.io-client";
-
-// ProseMirror
-import {EditorState} from "prosemirror-state";
-import {EditorView} from "prosemirror-view";
-import {Step} from "prosemirror-transform"
-import {schema} from "prosemirror-schema-basic";
-import {undo, redo, history} from "prosemirror-history";
-import {keymap} from "prosemirror-keymap";
-import {baseKeymap} from "prosemirror-commands"
-import * as collab from "prosemirror-collab";
+import React from "react";
+import Editor from "./editor/Editor";
 
 import './App.css';
 
-const IO_ENDPOINT = '/';
-const SOCKET = socketIOClient(IO_ENDPOINT);
-
-class App extends Component {
-  editorView = null;
-
-  collabEditor(authority) {
-    let view = new EditorView(document.querySelector("#editor"), {
-      state: EditorState.create({
-        doc: schema.nodeFromJSON(authority.doc),
-        plugins: [
-          collab.collab({version: authority.steps.length}),
-          history(),
-          keymap({"Mod-z": undo, "Mod-Shift-z": redo}),
-          keymap(baseKeymap)
-        ]
-      }),
-      dispatchTransaction(transaction) {
-        let newState = view.state.apply(transaction);
-        view.updateState(newState);
-        let sendable = collab.sendableSteps(newState);
-        if (sendable) {
-          SOCKET.emit('FromClient', JSON.stringify({
-            version: sendable.version,
-            steps: sendable.steps.map(step => step.toJSON()) || [],
-            clientID: sendable.clientID
-          }));
-        }
-      }
-    });
-
-    return view;
-  }
-
-  componentDidMount() {
-    SOCKET.on("FromServer", newData => {
-      const jsonData = JSON.parse(newData);
-
-      if (!this.editorView) {
-        this.editorView = this.collabEditor(jsonData.authority);
-      } else {
-        this.editorView.dispatch(
-          collab.receiveTransaction(
-            this.editorView.state,
-            jsonData.steps.map(step => Step.fromJSON(schema, step)),
-            jsonData.clientIDs
-          )
-        );
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="view">
-        <p>Editor:</p>
-        <div className="editorWrapper" id="editor" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="view">
+    <h1>Collab Demo</h1>
+    <Editor />
+  </div>
+);
 
 export default App;
---
+++ client/src/editor/Editor.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+// ProseMirror
+import {EditorState} from "prosemirror-state";
+import {EditorView} from "prosemirror-view";
+import {Step} from "prosemirror-transform"
+import {schema} from "prosemirror-schema-basic";
+import {undo, redo, history} from "prosemirror-history";
+import {keymap} from "prosemirror-keymap";
+import { baseKeymap } from "prosemirror-commands";
+
+import * as collab from "prosemirror-collab";
+
+import { menu } from "./Menu";
+
+import './Editor.css';
+
+const IO_ENDPOINT = '/';
+const SOCKET = socketIOClient(IO_ENDPOINT);
+
+class Editor extends Component {
+  editorView = null;
+
+  collabEditor(authority) {
+    let view = new EditorView(document.querySelector("#editor"), {
+      state: EditorState.create({
+        doc: schema.nodeFromJSON(authority.doc),
+        plugins: [
+          collab.collab({version: authority.steps.length}),
+          history(),
+          keymap({"Mod-z": undo, "Mod-Shift-z": redo}),
+          keymap(baseKeymap),
+          menu
+        ]
+      }),
+      dispatchTransaction(transaction) {
+        let newState = view.state.apply(transaction);
+        view.updateState(newState);
+        let sendable = collab.sendableSteps(newState);
+        if (sendable) {
+          SOCKET.emit('FromClient', JSON.stringify({
+            version: sendable.version,
+            steps: sendable.steps.map(step => step.toJSON()) || [],
+            clientID: sendable.clientID
+          }));
+        }
+      }
+    });
+
+    return view;
+  }
+
+  componentDidMount() {
+    SOCKET.on("FromServer", newData => {
+      const jsonData = JSON.parse(newData);
+
+      if (!this.editorView) {
+        this.editorView = this.collabEditor(jsonData.authority);
+      } else {
+        this.editorView.dispatch(
+          collab.receiveTransaction(
+            this.editorView.state,
+            jsonData.steps.map(step => Step.fromJSON(schema, step)),
+            jsonData.clientIDs
+          )
+        );
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div
+        id="editor"
+        className="editorWrapper"
+        onClick={() => this.editorView.focus()}
+      />
+    );
+  }
+}
+
+export default Editor;
